6cf82a28d6b922a6c93797da935a836ce6d5aabb
Philipp <philippaltmann@deap.io>
2020-01-14 17:54:40 +0100

    Added inner & outer circle prolog calls

--- src/components/Prolog.vue
+++ src/components/Prolog.vue
@@ -20,12 +20,45 @@
     methods: {
       computeCircles: function(){
         let query = `circles(triangle(${this.triangle.map(p=>`point(${p.x}, ${p.y})`).join(', ')}), ${this.triangle.map(p=>p.name).join(', ')}).`;
-        this.session.query( query );
+        let parsed = this.session.query( query );
+        if( parsed !== true ) { console.log( parsed ); }
+        // console.log(query);
+        let circles = []
+        this.session.answer( a => {
+            // console.log(a.toString());
+            circles = this.triangle.map(t => Object.assign(t, {r: a.lookup(t.name).value}));
+            // this.$emit('circles-change', c);
+        });
+        // console.log(circles);
+
+        //Inner Circle
+        query = `inner(triangle(${this.triangle.map(p=>`point(${p.x}, ${p.y})`).join(', ')}), X, Y, R).`;
+        parsed = this.session.query( query );
+        if( parsed !== true ) { console.log( parsed ); }
         // console.log(query);
         this.session.answer( a => {
-            let c = this.triangle.map(t => Object.assign(t, {r: a.lookup(t.name).value}));
-            this.$emit('circles-change', c);
+            let circle = {x: a.lookup('X').value, y: a.lookup('Y').value, r: a.lookup('R').value}
+            circles.push(circle);
+            // console.log(a.toString());
+            // this.$emit('circles-change', c);
         });
+        // console.log(circles);
+
+        //Outer Circle
+        query = `outer(triangle(${this.triangle.map(p=>`point(${p.x}, ${p.y})`).join(', ')}), X, Y, R).`;
+        parsed = this.session.query( query );
+        if( parsed !== true ) { console.log( parsed ); }
+        // console.log(query);
+        this.session.answer( a => {
+            let circle = {x: a.lookup('X').value, y: a.lookup('Y').value, r: a.lookup('R').value}
+            circles.push(circle);
+            // console.log(a.toString());
+            // this.$emit('circles-change', c);
+        });
+        // console.log(circles);
+        this.$emit('circles-change', circles);
+
+
       }
     },
     watch: { triangle: function(t){ this.computeCircles()} },
