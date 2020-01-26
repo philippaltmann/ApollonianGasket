5e2b50ae3bbfc20a8c2bb7d73e239f93fa4bad73
Philipp <philippaltmann@deap.io>
2020-01-26 12:55:41 +0100

    Added Time Measures, changed draw/update data flow

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -29,14 +29,21 @@
         })
       },
       drawCircles: function(){
+        // let start = Date.now()
         this.circles.forEach(c => {
           this.context.beginPath();
           this.context.arc(c[0], c[1], Math.abs(c[2]), 0, Math.PI * 2, false);
           this.context.stroke()
         })
+        // const millis = Date.now() - start;
+        // console.log(`Drawing Depth 10 Gasket took: ${millis} MS`);
       }
     },
     watch: {
+      triangle: function () {
+        this.resetCanvas()
+        this.drawTriangle()
+      },
       circles: function () {
         this.resetCanvas()
         this.drawCircles()
@@ -53,13 +60,17 @@
       const matchPoint = (e,p) => (Math.abs(p.x-e.layerX) < this.padding && Math.abs(p.y-e.layerY) < this.padding)
 
       canvas.onmousedown = e => this.edit = this.triangle.findIndex(p => matchPoint(e, p));
-      canvas.onmouseup = () => this.edit = -1;
+      canvas.onmouseup = () => {
+        this.edit = -1;
+        this.$emit('update-gasket');
+      }
 
       canvas.onmousemove = (e) => {
         if(this.edit != -1){
           let triangle = this.triangle.splice(0)
           triangle[this.edit].x = e.layerX;
           triangle[this.edit].y = e.layerY;
+          // console.log(triangle);
           this.$emit('triangle-change', triangle)
         }
       };
