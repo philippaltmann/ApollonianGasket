a0d04ddb75fbf971e22988201179ff5931ea9fcb
Philipp <philippaltmann@deap.io>
2020-02-12 11:32:34 +0100

    Cleaned Prolog Calculations

--- src/prolog/apollonian.pl
+++ src/prolog/apollonian.pl
@@ -9,17 +9,9 @@ sides(((XA, YA), (XB, YB), (XC, YC)), DA, DB, DC) :-
   DB is sqrt((XA-XC)^2 + (YA-YC)^2),
   DC is sqrt((XA-XB)^2 + (YA-YB)^2).
 
-basis(triangle((XA, YA), (XB, YB), (XC, YC)), [[XA, YA, RA], [XB, YB, RB], [XC, YC, RC]]):-
-  sides(((XA, YA), (XB, YB), (XC, YC)), A, B, C),
-  RA is 1/2*(-A+B+C),
-  RB is 1/2*( A-B+C),
-  RC is 1/2*( A+B-C).
-
-basis((XA, YA, RA), (XB, YB, RB), (XC, YC, RC)):-
-  sides(((XA, YA), (XB, YB), (XC, YC)), A, B, C),
-  RA is 1/2*(-A+B+C),
-  RB is 1/2*( A-B+C),
-  RC is 1/2*( A+B-C).
+tangent((XA, YA), (XB, YB), (XC, YC), [[XA, YA, RA], [XB, YB, RB], [XC, YC, RC]]):-
+    sides(((XA, YA), (XB, YB), (XC, YC)), A, B, C),
+    RA is 1/2*(-A+B+C), RB is 1/2*( A-B+C), RC is 1/2*( A+B-C).
 
 
 % Complex Numbers
@@ -54,20 +46,21 @@ solve(C1, C2, C3, S) :-
 
 
 % adjacent((200, 200, -200), (100, 200, 100), (300, 200, 100), C4).
-adjacent((X1, Y1, R1), (X2, Y2, R2), (X3, Y3, R3), (X4, Y4, R4)) :-
+adjacent([X1, Y1, R1], [X2, Y2, R2], [X3, Y3, R3], [X4, Y4, R4]) :-
   % Calculagte Bends and solve for 4th Radius using Descartes
   B1 is 1/R1, B2 is 1/R2, B3 is 1/R3, % calculate bends
-  solve((B1, 0), (B2, 0), (B3, 0), (B4,_)), R4 is 1/B4, %if not working -> abs(1/B4)
+  solve((B1, 0), (B2, 0), (B3, 0), (B4,_)), R4 is 1/B4,
   % Calculate Relaitive Centers to Solve Descartes Equation
   solve((X1*B1, Y1*B1), (X2*B2, Y2*B2), (X3*B3, Y3*B3), BC4),
   div(BC4, (B4, 0), (X4, Y4)).
 
-flip((X1, Y1, R1), (X2, Y2, R2), (X3, Y3, R3), (X4, Y4, R4), (X5, Y5, R5)) :-
+
+flip([X1, Y1, R1], [X2, Y2, R2], [X3, Y3, R3], [X4, Y4, R4], [X5, Y5, R5]) :-
   % "So if we already have one value for b_4, we can just subtract it from
   % double the sum of the other three bends to find the second value of b_4."
 	% https://mathlesstraveled.com/2016/05/04/apollonian-gaskets-and-descartes-theorem/
   B1 is 1/R1, B2 is 1/R2, B3 is 1/R3, B4 is 1/R4, % calculate bends
-  B5 is 2 * (B1 + B2 + B3) - B4, R5 is 1/B5,
+  B5 is 2 * (B1 + B2 + B3) - B4, R5 is 1/B5, % calculate 5th bend & corresponding radius
 
   % "For each operation we compute the same formula twice, once for the bends and once
   % for the bend-center products. To recover the center of a circle, just divide the
@@ -76,27 +69,27 @@ flip((X1, Y1, R1), (X2, Y2, R2), (X3, Y3, R3), (X4, Y4, R4), (X5, Y5, R5)) :-
   sum([(X1*B1, Y1*B1), (X2*B2, Y2*B2), (X3*B3, Y3*B3)], S), mul(S, (2,0), S2),
   sub(S2, ((X4*B4, Y4*B4)), S2Sub), div(S2Sub, (B5, 0), (X5, Y5)).
 
-gasket(_, _, _, 0, []).
+gasket(_, 0, []).
 
-gasket(C1, C2, C3, Depth, [[XI, YI, RI], [XO, YO, RO]|Tail ]) :-
-  adjacent(C1, C2, C3, (XI, YI, RI)), flip(C1, C2, C3, (XI, YI, RI), (XO, YO, RO)),
+gasket([C1, C2, C3], Depth, [C1, C2, C3, CI, CO|Tail ]) :-
+  adjacent(C1, C2, C3, CI), flip(C1, C2, C3, CI, CO),
   DepthN is Depth - 1,
-  recurse(C1, C2, C3, (XI, YI, RI), InnerRecurse, DepthN), % Further Inner Circles
-  recurse(C1, C2, C3, (XO, YO, RO), OuterRecurse, DepthN), % Further Outer Circles
+  recurse(C1, C2, C3, CI, InnerRecurse, DepthN), % Further Inner Circles
+  recurse(C1, C2, C3, CO, OuterRecurse, DepthN), % Further Outer Circles
   append(InnerRecurse, OuterRecurse, Tail).
 
 % Recursion Break Condition
 recurse( _, _, _, _, [], 0).
 
 % CO: Fixed Circle
-recurse(C1, C2, C3, CO, [ [X5, Y5, R5], [X6, Y6, R6], [X7, Y7, R7]|Tail ], Depth) :-
+recurse(C1, C2, C3, CO, [ C5, C6, C7|Tail ], Depth) :-
   DepthN is Depth - 1,
-  flip(CO, C1, C2, C3, (X5, Y5, R5)),
-  flip(CO, C1, C3, C2, (X6, Y6, R6)),
-  flip(CO, C2, C3, C1, (X7, Y7, R7)), !,
-  recurse(C1, C2, CO, (X5, Y5, R5), Rec1, DepthN),
-  recurse(C1, C3, CO, (X6, Y6, R6), Rec2, DepthN),
-  recurse(C2, C3, CO, (X7, Y7, R7), Rec3, DepthN),
+  flip(CO, C1, C2, C3, C5),
+  flip(CO, C1, C3, C2, C6),
+  flip(CO, C2, C3, C1, C7), !,
+  recurse(C1, C2, CO, C5, Rec1, DepthN),
+  recurse(C1, C3, CO, C6, Rec2, DepthN),
+  recurse(C2, C3, CO, C7, Rec3, DepthN),
   append(Rec1, Rec2, PreTail),
   append(PreTail, Rec3, Tail).
 
