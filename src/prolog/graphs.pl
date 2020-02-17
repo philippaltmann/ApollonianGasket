fa916feea1e5ffc8658fb171f4ccb689f5a6ca64
madu <mailsoeffentlich@gmail.com>
2020-02-17 14:11:29 +0100

    Add dialog box

--- src/prolog/graphs.pl
+++ src/prolog/graphs.pl
@@ -1,6 +1,7 @@
 :- use_module(library(pce)).
 :- use_module(library(plot/barchart)).
 :- use_module(library(autowin)).
+
 :- consult(triangle).
 
 %test_barchart :-
@@ -16,6 +17,9 @@
 
 graph :-
     new(W, picture('Apollonian Gasket')),
+    new(P, dialog),			% create dialog panel
+
+	send(P, above, W),		% put panel below drawing-area
     send(W, display,
         new(@ci, circle(100)), point(25, 25)),
     send(W, display,
@@ -26,4 +30,27 @@ graph :-
     send(@ci, fill_pattern, colour(blue)),
     send(@cj, fill_pattern, colour(green)),
     send(@ck, fill_pattern, colour(orange)),
+
+    send(P, append, button(box, message(@prolog, add_box, P))),
+	send(P, append, button(ellipse, message(@prolog, add_ellipse, P))),
+	send(P, append, button(quit, message(P, destroy))),
+
     send(W, open).
+
+
+layoutdemo1 :-
+    new(D, dialog('Layout Demo 1')),
+    send(D, append,
+    new(BTS, dialog_group(buttons, group))),
+    send(BTS, gap, size(0, 30)),
+    send(BTS, append, button(add)),
+    send(BTS, append, button(rename), below),
+    send(BTS, append, button(delete), below),
+    send(BTS, layout_dialog),
+    send(D, append, new(LB, list_browser), right),
+    send(D, append, new(TI, text_item(name, ’’))),
+    send(LB, alignment, left),
+    send(D, layout),
+    send(LB, bottom_side, BTS?bottom_side),
+    send(LB, right_side, TI?right_side),
+    send(D, open).
